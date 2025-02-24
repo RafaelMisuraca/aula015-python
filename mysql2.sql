1b6ff4fd184883a29133a2d060a66978cc5cde38
RafaelMisuraca <rafaelmisuraca@gmail.com>
2025-02-24 18:37:45 -0300

    Add files via upload

---
+++ mysql2.sql
@@ -0,0 +1,23 @@
+create database	escola;
+use escola;
+
+CREATE TABLE alunos(
+id_aluno int,
+nome varchar(40),
+idade int
+);
+
+CREATE TABLE cursos(
+id_cursos int,
+nome_curso varchar(40),
+carga_horaria int
+);
+
+CREATE TABLE matriculas(
+id_matricula int,
+id_aluno int,
+id_curso int,
+data_matricula int
+);
+
+TRUNCATE TABLE matriculas
